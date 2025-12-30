a5363ac2c0de440828d71a1ebf805c90870d96c7
Libertrendz <gustavo.santos@libertrendz.com.br>
2025-12-30 17:52:11 +0000

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/**
+ * =============================================
+ * Moduz+ | PostCSS
+ * Arquivo: postcss.config.js
+ * Etapa: Tailwind pipeline (v1)
+ * =============================================
+ */
+
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
